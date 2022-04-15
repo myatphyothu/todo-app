6dcee7544461bcf9ca1259435705cbbca31a5d09
myatphyothu <phyothu.myat@gmail.com>
2022-04-15 16:47:03 +0800

    commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-from models import db, Task
+from models import db, Task, Status, TASK_STATUS
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
@@ -24,7 +24,33 @@ def add_task():
 @app.route('/update_task/<int:task_id>')
 def update_task(task_id):
     task = Task.query.filter_by(id=task_id).first()
-    task.completed = not task.completed
+    if task.status == TASK_STATUS[0]:
+        task.status = TASK_STATUS[1]
+    elif task.status == TASK_STATUS[1]:
+        task.status = TASK_STATUS[2]
+
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/progress_task/<int:task_id>')
+def progress_task(task_id):
+    task = Task.query.filter_by(id=task_id).first()
+    if task.status == TASK_STATUS[0]:
+        task.status = TASK_STATUS[1]
+    elif task.status == TASK_STATUS[1]:
+        task.status = TASK_STATUS[2]
+
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/regress_task/<int:task_id>')
+def regress_task(task_id):
+    task = Task.query.filter_by(id=task_id).first()
+    if task.status == TASK_STATUS[2]:
+        task.status = TASK_STATUS[1]
+    elif task.status == TASK_STATUS[1]:
+        task.status = TASK_STATUS[0]
+
     db.session.commit()
     return redirect(url_for('index'))
 
@@ -37,8 +63,14 @@ def delete_task(task_id):
 
 
 def create_sample_task():
-    new_task = Task(title='An example task', completed=False)
+    new_task = Task(title='An example task', status=TASK_STATUS[0])
     db.session.add(new_task)
+    db.session.commit()
+
+def create_status():
+    for status in TASK_STATUS:
+        print(status)
+        db.session.add(Status(name=status))
     db.session.commit()
 
 if __name__ == '__main__':
@@ -47,5 +79,6 @@ if __name__ == '__main__':
     db.create_all()
 
     #create_sample_task()
+    #create_status()
 
     app.run(debug=True)
--- models.py
+++ models.py
@@ -2,10 +2,19 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
+TASK_STATUS = ('Not started', 'In progress', 'Completed')
+
+class Status(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
-    completed = db.Column(db.Boolean)
+    #completed = db.Column(db.Boolean)
+    status = db.Column(db.String(50), db.ForeignKey(Status.name))
 
 
 
